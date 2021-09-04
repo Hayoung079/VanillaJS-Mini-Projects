ed7a909c516c2fb6e2892adc7308bb61a52c4456
Hayoung079 <gksyddl22@gmail.com>
2021-09-04 09:26:05 +0900

    js - 주석 처리

--- clock/script.js
+++ clock/script.js
@@ -2,17 +2,19 @@ const body = document.querySelector('body');
 const hr = document.querySelector('.hr');
 const mn = document.querySelector('.mn');
 const sc = document.querySelector('.sc');
-const deg = 6;
 
-setInterval(() => {
+setInterval(() => {  
   let day = new Date();
-  let hh = day.getHours() * 30;
-  let mm = day.getMinutes() * deg;
-  let ss = day.getSeconds() * deg;
 
-  hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
-  mn.style.transform = `rotateZ(${mm}deg)`;
-  sc.style.transform = `rotateZ(${ss}deg)`;
+  // 1시간 당 돌아가는 각도 30° 1분,1초 당 돌아가는 각도 6°
+  let hourDeg = day.getHours() * 30;  
+  let minDeg = day.getMinutes() * 6;  
+  let secDeg = day.getSeconds() * 6; 
+
+  hr.style.transform = `rotateZ(${hourDeg+(minDeg/12)}deg)`;  
+  // minDeg/12 : 자연스러운 clock 느낌을 내기 위해 시침이 시간 사이에 위치하도록 조정
+  mn.style.transform = `rotateZ(${minDeg}deg)`;
+  sc.style.transform = `rotateZ(${secDeg}deg)`;
 })
 
 function toggleClass() {
