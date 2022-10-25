20699a119ded24b6574086df06a68e2926de682d
happai079 <gksyddl22@gmail.com>
2022-10-26 02:31:50 +0900

    feat: create character & platforms

---
+++ doodle-jump/app.js
@@ -0,0 +1,54 @@
+// DOM
+const grid = document.querySelector('.grid');
+const doodler = document.createElement('div');
+
+// Variables
+let doodlerLeftSpace = 50;
+let doodlerBottomSpace = 150;
+let isGameOver = false;
+let platformCount = 5;
+let platforms = [];
+
+// Create Character
+function createDoodler() {
+	grid.appendChild(doodler);
+	doodler.classList.add('doodler');
+	doodler.style.left = doodlerLeftSpace + 'px';
+	doodler.style.bottom = doodlerBottomSpace + 'px';
+}
+
+class Platform {
+	constructor(newplatBottom) {
+		// platform position
+		this.bottom = newplatBottom;
+		this.left = Math.random() * 315;
+
+		// platform element
+		this.visual = document.createElement('div');
+
+		const visual = this.visual;
+		visual.classList.add('platform');
+		visual.style.left = this.left + 'px';
+		visual.style.bottom = this.bottom + 'px';
+		grid.appendChild(visual);
+	}
+}
+
+// Create Platforms
+function createPlatforms() {
+	for (let i = 0; i < platformCount; i++) {
+		let platGap = 600 / platformCount;
+		let newplatBottom = 100 + i * platGap;
+		let newPlatform = new Platform(newplatBottom);
+		platforms.push(newPlatform);
+	}
+}
+
+// Start
+function start() {
+	if (!isGameOver) {
+		createDoodler();
+		createPlatforms();
+	}
+}
+start();
